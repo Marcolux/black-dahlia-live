dd755c71fc0d1998b20dc1d661fe23f089162dd6
Marcolux <mccolucci1@gmail.com>
2025-10-17 22:51:34 -0500

    Added CREATIVE on home page

--- react_website/src/pages/HomePage/HomePage.tsx
+++ react_website/src/pages/HomePage/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
         <div className="page flex flex-justifyContent-center flex-alignItems-center">
             <img src="/images/Dahlia_icon 2.svg" alt="logo"  style={{width: '300px', height: 'auto'}}  className="spin"/>
 
-            <h1 className="fontSize50">COMING SOON BLACK DAHLIA </h1>
+            <h1 className="fontSize50">COMING SOON - BLACK DAHLIA CREATIVE -</h1>
                 
             <h4 className="fontSize30 mt-10">Website under construction</h4>
         </div>
